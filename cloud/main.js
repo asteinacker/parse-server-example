68e2de4f4069e57f10bd7d45f0bc89a4ea78dfc7
asteinacker <31010009+asteinacker@users.noreply.github.com>
2017-08-14 13:45:59 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,21 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('setProjectAssignmentsToFalse', function(req, res) {
+  
+  const query = new Parse.Query("Review");
+  query.equalTo(req.params.header, true);
+  .find()
+  .then((results) => {
+   let value = false;
+    for (let i = 0, i < results.length; ++i) {
+      results[i].set(req.params.header, false);
+      results[i].save();
+    }
+    res.success("completed");
+  })
+  .catch(() => {
+    res.error("Column Update Failed");
+  });
+});
